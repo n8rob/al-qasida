e14a9b20e8e203781feecac64012c22c2aa5b2d0
Nathaniel Robinson <n8rrobinson@gmail.com>
2025-04-22 00:39:18 -0400

    package versions

--- setup.sh
+++ setup.sh
@@ -1,14 +1,18 @@
-pip install torch
-pip install transformers
+#!/bin/bash
+set -x
+pip install matplotlib
+pip install scikit-learn
+pip install scipy==1.15.2
+pip install transformers==4.50.0
+pip install datasets==3.3.2
 conda install conda-forge::fasttext
-pip install sacrebleu
+pip install sacrebleu==2.5.1
 #pip install pandas==2.1.1
-pip install tiktoken
-pip install sentencepiece
+pip install tiktoken==0.9.0
+pip install sentencepiece==0.2.0
 pip install google
 pip install google-api-python-client
-pip uninstall numpy
-pip install numpy 
-pip install scipy
-pip install pandas
+pip install torch==2.6.0
+pip install pandas==2.2.3
+pip install numpy==2.2.5 
 
